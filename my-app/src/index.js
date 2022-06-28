b3693e309fd8caf0bd9aa0ee41b8cbd63c467e1a
Anik2069 <anikislam668@gmail.com>
2022-06-29 01:51:03 +0600

    Time and date

---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const index = 0;
+setInterval(() => {
+  const element = (
+    <h1 className="heading" tabIndex={index}>
+       <span>Date {new Date().getDate()}-{new Date().getMonth()}-{new Date().getFullYear()}</span> <br/>
+      <span>Time {new Date().toLocaleTimeString()}</span>
+    </h1>
+  );
+
+
+  ReactDOM.render(element, document.getElementById('root'));
+}, 1000);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
