ec7f205b2502aac53e0c7016b7631d1bcc3e98c1
Anik2069 <anikislam668@gmail.com>
2022-07-09 15:18:34 +0600

    Element to Componet Convert

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const index = 0;
-setInterval(() => {
-  const element = (
-    <h1 className="heading" tabIndex={index}>
+//Element Use
+
+// const index = 0;
+// setInterval(() => {
+//   const element = (
+//     <h1 className="heading" tabIndex={index}>
+//        <span>Date {new Date().getDate()}-{new Date().getMonth()}-{new Date().getFullYear()}</span> <br/>
+//       <span>Time {new Date().toLocaleTimeString()}</span>
+//     </h1>
+//   );
+
+//   ReactDOM.render(element, document.getElementById('root'));
+// }, 1000);
+
+
+//Component
+
+function Clock({locale}){
+  return (
+    <h1 className="heading">
        <span>Date {new Date().getDate()}-{new Date().getMonth()}-{new Date().getFullYear()}</span> <br/>
-      <span>Time {new Date().toLocaleTimeString()}</span>
+      <span>Time {new Date().toLocaleTimeString(locale)}</span>
     </h1>
   );
+}
+
+//One Way
+//ReactDOM.render(Clock(), document.getElementById('root'));
+//2nd Way 
+ReactDOM.render(<Clock locale="bn-BD"/>, document.getElementById('root'));
 
 
-  ReactDOM.render(element, document.getElementById('root'));
-}, 1000);
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
